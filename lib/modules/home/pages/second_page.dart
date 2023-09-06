e9e2eace4080cbcddca28336e89681b6fbaae565
rafaelcostadesenv <rafael.costa.desenv@gmail.com>
2023-09-06 19:19:06 -0300

    Aula 05/09

---
+++ lib/modules/home/pages/second_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.amber,
+        title: const Text('SecondPage'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.add),
+              Text(
+                'TITULO SECOND PAGE',
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+          const Text('Segundo Texto'),
+          ElevatedButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: const Text('Voltar'),
+          ),
+        ],
+      ),
+    );
+  }
+}
